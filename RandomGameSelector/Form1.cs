b696e50f8bf7a14a6000761b31ede48cc2bd8914
ForgetfulShrug <frasierparrish@gmail.com>
2024-04-04 11:32:45 -0400

    Implemented Read Input Settings File - ForgetfulShrug 4/4/24

--- RandomGameSelector/Form1.cs
+++ RandomGameSelector/Form1.cs
@@ -51,20 +51,25 @@ namespace RandomGameSelector
             {
                 //Pass the filepath and filename to the StreamWriter Constructor
                 StreamWriter sw = new StreamWriter("Selector_Settings.txt");
-                sw.WriteLine("Filename: Test_Game_Lists.xlsx");
+                sw.WriteLine("Filename:Test_Game_Lists.xlsx");
                 sw.WriteLine("Sheet:Games_List");
                 sw.WriteLine("");
                 sw.WriteLine("Column 1:Recordable");
                 sw.WriteLine("Input 1:yes");
                 sw.WriteLine("");
-                sw.WriteLine("Column 2:Physical/Digital");
-                sw.WriteLine("Input 2:Digital");
+                sw.WriteLine("Column 2:System");
+                sw.WriteLine("Input 2:Switch");
                 sw.WriteLine("");
-                sw.WriteLine("Column 3:NA");
-                sw.WriteLine("Input 3:NA");
+                sw.WriteLine("Column 3:Digital/Physical");
+                sw.WriteLine("Input 3:Digital");
+                sw.WriteLine("");
+                sw.WriteLine("Column 4:AND");
+                sw.WriteLine("Input 4:Both");
                 sw.WriteLine("");
                 sw.WriteLine("Place information from the xlsx file for the Filename, Sheet Name, Column name and required content of the cell.");
-                sw.WriteLine("Put NA for ignored options except for the first four.");
+                sw.WriteLine("Put NA for ignored Columns except for the first");
+                sw.WriteLine("Put AND for Column 4 for OR Operator with 3/4 options");
+                sw.WriteLine("Input xlsx needs Title and Console in first two rows");
 
                 //Close the file
                 sw.Close();
@@ -81,23 +86,28 @@ namespace RandomGameSelector
         }
 
 
-        public string PullSettings() {
-            String settings;
+        public string[] PullSettings() {
+            String[] Settings = new string[10];
             try
             {
                 //Pass the file path and file name to the StreamReader constructor
                 StreamReader sr = new StreamReader("Selector_Settings.txt");
-                //Read the first line of text
-                settings = sr.ReadLine();
-                //Continue to read until you reach end of file
-                while (settings != null)
-                {
-                    //write the line to console window
-                    Console.WriteLine(settings);
-                    //Read the next line
-                    settings = sr.ReadLine();
-                }
-                //close the file
+                //Read Filename, Sheet, Columns, and required Cell contents
+                Settings[0] = sr.ReadLine().Replace("Filename:","").TrimStart(' ');
+                Settings[1] = sr.ReadLine().Replace("Sheet:", "").TrimStart(' ');
+                 sr.ReadLine();
+                Settings[2] = sr.ReadLine().Replace("Column 1:", "").TrimStart(' ');
+                Settings[3] = sr.ReadLine().Replace("Input 1:", "").TrimStart(' ');
+                 sr.ReadLine();
+                Settings[4] = sr.ReadLine().Replace("Column 2:", "").TrimStart(' ');
+                Settings[5] = sr.ReadLine().Replace("Input 2:", "").TrimStart(' ');
+                 sr.ReadLine();
+                Settings[6] = sr.ReadLine().Replace("Column 3:", "").TrimStart(' ');
+                Settings[7] = sr.ReadLine().Replace("Input 3:", "").TrimStart(' ');
+                sr.ReadLine();
+                Settings[8] = sr.ReadLine().Replace("Column 4:", "").TrimStart(' ');
+                Settings[9] = sr.ReadLine().Replace("Input 4:", "").TrimStart(' ');
+
                 sr.Close();
                 Console.ReadLine();
             }
@@ -109,14 +119,16 @@ namespace RandomGameSelector
             {
                 Console.WriteLine("Executing finally block.");
             }
-            return "setting";
+            return Settings;
         }
 
 
         private void PullGameLists()
         {
 
-            PullSettings();
+            //Initialize Settings;
+            String[] Settings = new string[10];
+            Settings = PullSettings();
 
 
             ////Was from working with IronExcel but was slow and paid 
@@ -128,26 +140,86 @@ namespace RandomGameSelector
             try
             {
                 //Replace path with path to 
-                var path = @"Test_Game_Lists.xlsx";
-                var dataList = ExcelUtility.ExcelDataToDataTable(path, "Games_List", true);
+                var path = Settings[0];
+                var dataList = ExcelUtility.ExcelDataToDataTable(path, Settings[1], true);
 
-                //remove unrecordable, Got this from somewhere don't remember where
+
+
+                // Column 1 Check for Cell input and delete lacking rows
                 for (int i = dataList.Rows.Count - 1; i >= 0; i--)
                 {
                     DataRow dr = dataList.Rows[i];
-                    if (dr["Recordable"].ToString() != "yes")
+                    if (dr[Settings[2]].ToString() != Settings[3])
                         dr.Delete();
                 }
                 dataList.AcceptChanges();
 
+
+                //Check if NA for 2nd Column and if not check more Cells for input column
+                if (Settings[4] != "NA") {
+
+                    for (int i = dataList.Rows.Count - 1; i >= 0; i--)
+                    {
+                        DataRow dr = dataList.Rows[i];
+                        if (dr[Settings[4]].ToString() != Settings[5])
+                            dr.Delete();
+                    }
+
+                    dataList.AcceptChanges();
+
+                }
+
+
+                //Check if NA/AND for 3rd Column and if not check more Cells for input column
+                if (Settings[6] != "NA" & Settings[8] != "AND")
+                {
+
+                    for (int i = dataList.Rows.Count - 1; i >= 0; i--)
+                    {
+                        DataRow dr = dataList.Rows[i];
+                        if (dr[Settings[6]].ToString() != Settings[7])
+                            dr.Delete();
+                    }
+                    dataList.AcceptChanges();
+
+                }
+                // If AND then check cell contents of column 3 against both inputs 3 and 4
+                else if (Settings[6] == "AND") {
+                    for (int i = dataList.Rows.Count - 1; i >= 0; i--)
+                    {
+                        DataRow dr = dataList.Rows[i];
+                        if (dr[Settings[6]].ToString() != Settings[7] | dr[Settings[6]].ToString() != Settings[9] )
+                            dr.Delete();
+                    }
+                    dataList.AcceptChanges();
+                }
+
+                // If not NA or AND then remove rows of cell not equal to input
+                if (Settings[8] != "NA" & Settings[8] != "AND")
+                {
+
+                    for (int i = dataList.Rows.Count - 1; i >= 0; i--)
+                    {
+                        DataRow dr = dataList.Rows[i];
+                        if (dr[Settings[8]].ToString() != Settings[9])
+                            dr.Delete();
+                    }
+
+                    dataList.AcceptChanges();
+
+                }
+
+
+                // Remove Excess Columns
                 while (dataList.Columns.Count > 3) // Remove Columns past #
                 {
                     dataList.Columns.RemoveAt(3);
                 }
 
-                ////Originally used dataGrids to show data
-                //dataGridView.Visible = true;
+                
 
+
+                //Add Duplicate Prevention
                 //Generating the random number and need the same for each row
                 int RowNum = dataList.Rows.Count;
                 Random rnd = new Random();
@@ -159,11 +231,6 @@ namespace RandomGameSelector
                 label1.Text = dataList.Rows[Bob][0].ToString() + " \n" + dataList.Rows[Bob][1].ToString();
                 label2.Text = dataList.Rows[Bobb][0].ToString() + " \n" + dataList.Rows[Bobb][1].ToString();
                 label3.Text = dataList.Rows[Bobby][0].ToString() + " \n" + dataList.Rows[Bobby][1].ToString();
-
-
-                //button2.Text = dataList.Rows[Bobb][0].ToString() + " \n" + dataList.Rows[Bobb][1].ToString();
-                //= dataList.Rows[Bobb][0].ToString() + " \n" + dataList.Rows[Bobb][1].ToString();           
-                //dataGridView.DataSource = dataList;
 
             }
             catch (Exception ex)
